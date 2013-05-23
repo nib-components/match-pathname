16783527205a5fb5de23c283f293cf11691e5b5d
Chris Buttery <chrisbuttery@Chriss-MacBook-Pro.local>
2013-05-23 16:44:53 +1000

    Removed ternary due to IE spazzing out

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 module.exports = function(selector, className) {
-    Array.prototype.forEach.call(document.body.querySelectorAll(selector), function(el){
-        el.classList.toggle(className, el.getAttribute('data-url') === window.location.pathname);
-    });
+  Array.prototype.forEach.call(
+    document.body.querySelectorAll(selector), 
+    function(el){
+      if(el.getAttribute('data-url') === window.location.pathname) {
+        el.classList.toggle(className);
+      }
+    }
+  );
 };
