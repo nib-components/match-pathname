47da6669c9b88034ba9a4af76a05f073d6eb6063
chrisbuttery <info@chrisbuttery.com>
2013-08-06 12:55:39 +1000

    Added component/dom for easy ie8 fallback

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+var dom = require('dom');
+
 module.exports = function(selector, className) {
   Array.prototype.forEach.call(
-    document.body.querySelectorAll(selector), 
-    function(el){
-      if(el.getAttribute('data-url') === window.location.pathname) {
-        el.classList.toggle(className);
+    document.body.querySelectorAll(selector),
+    function(selector){
+      if(selector.getAttribute('data-url') === window.location.pathname) {
+        dom(selector).toggleClass(className);
       }
     }
   );
