2906c6bc04b6d22e16877fba8ec8ce9a9cb13f0a
Chris Buttery <info@chrisbuttery.com>
2013-08-23 17:50:28 +1000

    ie friendly
    So remove the class from the element, and if data-url matches window.location.pathname, add it.

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ var doc = document;
 
 module.exports = function(selector, className) {
    each.call(doc.querySelectorAll(selector), function(el){
-      el.classList.toggle(className, el.getAttribute('data-url') === window.location.pathname);
+      el.classList.remove(className);
+      if (el.getAttribute('data-url') === window.location.pathname){
+        el.classList.add(className);
+      }
    });
 }
